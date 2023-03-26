b81aabc63763bc8be7374234befd10db05c592ce
Aparna Krishnan <aparna.k799@gmail.com>
2023-03-26 15:40:38 -0700

    Initial frontend

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div classname="navbar">
+        <div className='leftSide'></div>
+        <div className='rightSide'></div>
+    </div>
+  )
+}
+
+export default Navbar
