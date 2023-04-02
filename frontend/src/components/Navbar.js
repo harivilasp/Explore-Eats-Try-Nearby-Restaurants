d605af8c78ed3e163fca29261d66c78557fefc75
Aparna Krishnan <aparna.k799@gmail.com>
2023-04-02 01:15:40 -0700

    nav

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import ExploreEats_flat from '../assets/ExploreEats_flat.png'
 function Navbar() {
   return (
-    <div classname="navbar">
-        <div className='leftSide'></div>
+    <div className="navbar">
+      <div style={{color:'red'}}>
+        <div className='leftSide'>
+            <img style={{width: '50', height: '50', float: 'left'}} src={ExploreEats_flat}/>
+        </div>
         <div className='rightSide'></div>
+        </div>
     </div>
   )
 }
