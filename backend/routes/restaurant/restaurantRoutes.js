082dc8de44436a6b2c947e5e030f8151c63939bf
Akshat Gandhi <akshat@Akshats-MacBook-Air.local>
2023-04-25 21:58:52 -0400

    profile edit

--- backend/routes/restaurant/restaurantRoutes.js
+++ backend/routes/restaurant/restaurantRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const router = express.Router()
-const {registerRestaurant, loginRestaruant, getMe} = require('../../controllers/restaurantController')
+const {registerRestaurant, loginRestaruant, getMe, updateMe} = require('../../controllers/restaurantController')
 const {protectRestaurant} = require('../../middleware/restaurantAuthMiddleware')
 
 
@@ -9,5 +9,5 @@ const {protectRestaurant} = require('../../middleware/restaurantAuthMiddleware')
 router.post('/', registerRestaurant) // Adding a restaurant ==> registration
 router.post('/login', loginRestaruant) // authenticate a restaurant ==> login
 router.get('/me',protectRestaurant, getMe) // get the restaurant data ==> login // Added protect middleware, so before doing get me it will first verify the token using the protect function
-
+router.post('/profile', protectRestaurant, updateMe)
 module.exports = router
