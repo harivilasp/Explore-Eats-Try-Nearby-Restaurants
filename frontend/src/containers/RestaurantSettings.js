c956d0d6bc14b87ef0a8c874fa349d40fdd2b0f7
harivilasp <harivilasp@gmail.com>
2023-04-26 17:28:06 -0400

    completed restaurant update

--- frontend/src/containers/RestaurantSettings.js
+++ frontend/src/containers/RestaurantSettings.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
+import { login } from "../redux/slices/authSlice";
+import axios from "axios";
 
 function RestaurantSettings() {
+  const dispatch = useDispatch();
+
   const user = useSelector((state) => state.auth.user);
   // Change name state
   const [name, setName] = useState(user.user.name);
@@ -10,9 +14,42 @@ function RestaurantSettings() {
   const [phoneNumber, setPhoneNumber] = useState(user.user.phoneNumber);
   // Change Address
   const [address, setAddress] = useState(user.user.address);
+
+  const handleUpdateProfile = async () => {
+    try {
+      const config = {
+        headers: { Authorization: `Bearer ${user.user.token}` },
+      };
+      const body = {
+        name: name,
+        phoneNumber: phoneNumber,
+        address: address,
+      };
+      const res = await axios.post(
+        "http://localhost:5001/api/restaurants/profile",
+        body,
+        config
+      );
+      // console.log(res.data);
+      // console.log(user);
+      let payload = {
+        ...user,
+        user: {
+          ...user.user,
+          name: res.data.name,
+          phoneNumber: res.data.phoneNumber,
+          address: address,
+        },
+      };
+      dispatch(login(payload));
+      // Optionally, show a success message to the user
+    } catch (error) {
+      // Handle errors, e.g. show an error message to the user
+    }
+  };
   return (
     <div className="container mx-auto px-4 py-8">
-      <pre>{JSON.stringify(user, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(user, null, 2)}</pre> */}
       <h1 className="text-3xl font-bold mb-8">Edit Profile</h1>
 
       <div className="grid grid-cols-2 gap-8">
@@ -56,6 +93,12 @@ function RestaurantSettings() {
               />
             </li>
           </ul>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            onClick={handleUpdateProfile}
+          >
+            Save Changes
+          </button>
         </div>
         <div className="col-span-2 md:col-span-1">
           <h2 className="text-xl font-bold mb-4">Account Information</h2>
