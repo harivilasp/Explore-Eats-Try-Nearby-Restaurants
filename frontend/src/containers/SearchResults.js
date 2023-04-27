2b5b6e20c2f80291b5b57ebd9a5249db4ab8f256
Aparna Krishnan <aparna.k799@gmail.com>
2023-04-27 04:00:17 -0700

    merge didnt work, manual update

--- frontend/src/containers/SearchResults.js
+++ frontend/src/containers/SearchResults.js
@@ -1,35 +1,34 @@
-// make component to display restaurant info
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Card from "../components/Card";
 
 function SearchResults() {
-  const [result, setResult] = useState({});
-  // const [loading, setLoading] = useState(true);
+  const [result, setResult] = useState([]);
+
   const { searchTerm } = useParams();
+
   useEffect(() => {
-    console.log("inside useEffect");
     axios
       .get(`http://localhost:5001/api/search/${searchTerm}`)
       .then((res) => res.data)
       .then((data) => {
         console.log(data);
         setResult(data);
-        // setLoading(false);
       });
   }, [searchTerm]);
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+
+  if (!result || result.length === 0) {
+    return <div>No results found</div>;
+  }
+
   return (
-    <pre>{JSON.stringify(result, null, 2)}</pre>
-    // <div className="restaurant">
-    //   <h1>{restaurant.name}</h1>
-    //   <p>{restaurant.email}</p>
-    //   <p>{restaurant.phoneNumber}</p>
-    //   <p>{restaurant.address}</p>
-    //   <p>{restaurant.status}</p>
-    // </div>
+    <div className="flex">
+           {" "}
+      {result.map((restaurant) => (
+        <Card key={restaurant.place_id} restaurant={restaurant} />
+      ))}
+    </div>
   );
 }
 
