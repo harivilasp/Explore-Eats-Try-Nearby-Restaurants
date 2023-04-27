ab4f52233844f21990c300b549f1b79581698caf
Aparna Krishnan <aparna.k799@gmail.com>
2023-04-27 10:00:18 -0700

    final push

--- frontend/src/containers/RestaurantHome.js
+++ frontend/src/containers/RestaurantHome.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/RestaurantHome.css'; // import the CSS file for styling
+import { useDispatch, useSelector } from 'react-redux';
+import { getRestaurantPendingCalls, setPendingCalls } from '../redux/slices/authSlice';
 
 function RestaurantHome() {
   // create some dummy data for the tables
   const followers = ['John', 'Jane', 'Jack', 'Jill'];
-  const callbacks = [
-    { name: 'Joe', phone: '123-456-7890', email: 'joe@example.com' },
-    { name: 'Kate', phone: '987-654-3210', email: 'kate@example.com' },
-    { name: 'Mike', phone: '555-555-5555', email: 'mike@example.com' },
-  ];
+//   const callbacks = [
+//     { name: 'Joe', phone: '123-456-7890', email: 'joe@example.com' },
+//     { name: 'Kate', phone: '987-654-3210', email: 'kate@example.com' },
+//     { name: 'Mike', phone: '555-555-5555', email: 'mike@example.com' },
+//   ];
+  const token = useSelector(state => state.auth.user.user.token)
+  const dispatch = useDispatch()
+  const callbacks = useSelector(state => state.auth.pendingCalls)
+
+  const getPendingCallBacks = () =>{
+    console.log({token})
+    dispatch(getRestaurantPendingCalls(token))
+  }
+  useEffect(() =>{
+
+getPendingCallBacks()
+
+  },[])
 
   // state to keep track of completed callbacks
   const [completedCallbacks, setCompletedCallbacks] = useState([]);
@@ -16,30 +31,20 @@ function RestaurantHome() {
   // function to handle completing a callback
   const handleCompleteCallback = (index) => {
     // remove the completed callback from the callbacks array
-    const callbackToRemove = callbacks.splice(index, 1)[0];
+    const callbackToRemove = [...callbacks].splice(index, 1)[0];
+    // console.log({callbackToRemove})
+    const data = [...callbacks].filter(ele => ele.id !== callbackToRemove.id)
+    dispatch(setPendingCalls(data))
 
     // add the completed callback to the completedCallbacks array
     setCompletedCallbacks([...completedCallbacks, callbackToRemove]);
+
   };
 
   return (
     <div className="favorites-container">
       <div className="table-container">
-        <h2>Followers</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {followers.map((follower, index) => (
-              <tr key={index}>
-                <td>{follower}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        
       </div>
       <div className="table-container">
         <h2>Call Backs</h2>
@@ -48,7 +53,7 @@ function RestaurantHome() {
             <tr>
               <th>Name</th>
               <th>Phone</th>
-              <th>Email</th>
+     
               <th></th>
             </tr>
           </thead>
@@ -57,7 +62,7 @@ function RestaurantHome() {
               <tr key={index}>
                 <td>{callback.name}</td>
                 <td>{callback.phone}</td>
-                <td>{callback.email}</td>
+   
                 <td>
                   <button onClick={() => handleCompleteCallback(index)}>
                     Completed Call Back
@@ -75,7 +80,7 @@ function RestaurantHome() {
             <tr>
               <th>Name</th>
               <th>Phone</th>
-              <th>Email</th>
+              
             </tr>
           </thead>
           <tbody>
@@ -83,7 +88,7 @@ function RestaurantHome() {
               <tr key={index}>
                 <td>{callback.name}</td>
                 <td>{callback.phone}</td>
-                <td>{callback.email}</td>
+                
               </tr>
             ))}
           </tbody>
